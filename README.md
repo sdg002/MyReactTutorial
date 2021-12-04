5fbbf4f4c7002f1bbb0f8463ed0f01635ee21569
saurabh <saurabh_dasgupta@hotmail.com>
2021-12-04 22:00:10 +0000

    Clock2 - useEffect for 1-time initialization

--- README.md
+++ README.md
@@ -13,3 +13,16 @@ This is a project which represents my learning and experiments with React
 
 ![Simple clock!](images/simple_clock.png "Bootstrap experiments")
 
+# How to do a one-time initialization in your component?
+Read this StackOverFlow post
+https://stackoverflow.com/questions/53120972/how-to-call-loading-function-with-react-useeffect-only-once
+
+You are passing an `useEffect` handler with an empty array.
+
+```
+    const onloadHandler=()=>{
+        console.log("CLOCK2: onLoadHandler")
+    }
+    rc.useEffect(onloadHandler,[]);
+
+```
--- simple-clock-high-order/src/components/Clock2.js
+++ simple-clock-high-order/src/components/Clock2.js
@@ -6,23 +6,32 @@ const clockIntervalMs=1000; //ms
 const Clock2 = () => {
     const clockStyle={ backgroundColor:"ghostwhite"}
     const [currentTime,setTime]=rc.useState(null)
+    const [isInitialized,setIsInitialized]=rc.useState(false)
     const [javascriptClockIntervalObject,setJavascriptClockIntervalObject]=rc.useState(null)
 
     let startClockHandler=null;
     let stopClockHandler=null;
 
     rc.useEffect(()=>{
-        console.log(`Inside useEffect window clock=${javascriptClockIntervalObject}`)
+        console.log(`CLOCK2:Inside dummy useEffect window clock=${javascriptClockIntervalObject}`)
     });
 
+    const onloadHandler=()=>{
+        console.log("CLOCK2: onLoadHandler");
+        startClockHandler();
+    }
+    rc.useEffect(onloadHandler,[]);
+    //https://stackoverflow.com/questions/53120972/how-to-call-loading-function-with-react-useeffect-only-once
+
+
     stopClockHandler=()=>{
-        console.log(`inside stop handler click ${javascriptClockIntervalObject}`)
+        console.log(`CLOCK2:inside stop handler click ${javascriptClockIntervalObject}`)
         clearInterval(javascriptClockIntervalObject)
         setJavascriptClockIntervalObject(null)
     }
 
     startClockHandler=()=>{
-        console.log(`inside start handler click ${javascriptClockIntervalObject}`)
+        console.log(`CLOCK2:inside start handler click ${javascriptClockIntervalObject}`)
         let windowClock=setInterval(timeCallBack,clockIntervalMs);
         setJavascriptClockIntervalObject(windowClock)
     }
